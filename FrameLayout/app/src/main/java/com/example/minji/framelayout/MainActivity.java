581eb2f720196de7f62e8943a3499d6aba47021a
minji-PC\minji <govlwhdl@gmil.com>
2017-05-26 16:45:09 +0900

    FrameLayout을 이용하여 여러 개의 뷰를 배치하고 보여주기 . . 버튼을 누르면 그 버튼 색이 framelayout에 뜸

---
+++ FrameLayout/app/src/main/java/com/example/minji/framelayout/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.minji.framelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView redText;
+    TextView greenText;
+    TextView blueText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.red).setOnClickListener(this);
+        findViewById(R.id.green).setOnClickListener(this);
+        findViewById(R.id.blue).setOnClickListener(this);
+
+        redText= (TextView)findViewById(R.id.redText);
+        greenText= (TextView)findViewById(R.id.greenText);
+        blueText= (TextView)findViewById(R.id.blueText);
+    }
+    @Override
+    public void onClick(View v){
+        redText.setVisibility(View.INVISIBLE);
+        greenText.setVisibility(View.INVISIBLE);
+        blueText.setVisibility(View.INVISIBLE);
+
+        if(v.getId() ==R.id.red)
+            redText.setVisibility(View.VISIBLE);
+        if(v.getId() ==R.id.green)
+            greenText.setVisibility(View.VISIBLE);
+        if(v.getId() ==R.id.blue)
+            blueText.setVisibility(View.VISIBLE);
+    }
+}
